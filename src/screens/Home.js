d52fcf39264cb9afb50555bec0b0ea52d37e3fbb
lurdharry <azeezhammed57@gmail.com>
2020-03-16 18:56:02 +0100

    Added Dummy mealData to home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,12 +2,37 @@ import React, {Component} from 'react';
 import {View, StyleSheet,TouchableOpacity,Text,Image,TextInput,ScrollView } from 'react-native';
 import {widthPercentageToDP,heightPercentageToDP,} from 'react-native-responsive-screen'
 import {hp,wp} from './signIn'
-export default class Home extends Component {
 
+const TestMeal=[
+    {
+        name:'Cheese Burger',
+        topping:'Beef, Veggies & Chilli',
+        img:require('../assets/images/cart.png'),
+
+    },
+    {
+        name:'Jollof Rice',
+        topping:'Grilled Chicken, Veggies & Sauce',
+        img:require('../assets/images/cart.png')
+    },
+    {
+        name:'Cheese Burger',
+        topping:'Beef, Veggies & Chilli',
+        img:require('../assets/images/cart.png')
+    },
+
+]
+export default class Home extends Component {
+    state={
+        searchValue:''
+    }
   render() {
       const cart =  require('../assets/images/cart.png')
+      const searchIcon =  require('../assets/images/searchIcon.png')
+
     return (
       <View style={styles.container}>
+        <ScrollView>
         {/* start of cart Icon */}
         <TouchableOpacity style={styles.cartContainer}>
             <View/>
@@ -17,16 +42,69 @@ export default class Home extends Component {
         {/* end of cart icon */}
         <Text style={styles.name}>Hello, John</Text>
         <Text style={styles.info}>Select your meal for the day</Text>
+        {/* start of search box */}
+        <View style={styles.searchBox}>
+            <Image source={searchIcon} style={styles.searchIcon} resizeMode='contain' />
+            <TextInput
+                placeholder='search for meals, dishes'
+                placeholderTextColor='rgba(74, 74, 74, 0.521798)'
+                style={styles.searchInput}
+                value={this.state.searchValue}
+                onChangeText={(text)=>this.setState({searchValue:text})}
+            
+            />
+
+        </View>
+        {/* end of search box */}
+        <ScrollView>
+            {TestMeal.map((item,index)=>{
+                return(
+                    <TouchableOpacity style={styles.mealItem}>
+                        <Image source={item.img} resizeMode='contain' style={styles.mealImage}/>
+                    </TouchableOpacity>
+                )
+            })}
+            
+        </ScrollView>
+
+        </ScrollView>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
+    mealImage:{
+        width:wp(308),
+        height:(113/308)*wp(308)
+    },
+    mealItem:{
+        height:(192/308)*wp(308),
+        width:wp(308),
+        borderRadius:hp(9)
+    },
+    searchInput:{
+        width:wp(250),
+        marginLeft:wp(10)
+        
+    },
+    searchIcon:{
+        width:wp(15),
+        height:wp(15)
+    },
+    searchBox:{
+        width:wp(307),
+        height:(41/307)*wp(307),
+        backgroundColor:'#F6F6F6',
+        borderRadius:hp(9),
+        flexDirection:'row',
+        alignItems:'center',
+        paddingLeft:wp(16)
+    },
     info:{
         color:'#4A4A4A',
-        fontSize:hp(26),
+        fontSize:hp(14),
         fontWeight:'bold',
-        marginBottom:hp(8)
+        marginBottom:hp(24)
     },
     name:{
         color:'#4A4A4A',
